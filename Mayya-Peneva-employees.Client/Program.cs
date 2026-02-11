c075f9a29f36d3c7a2d7c45c6d23a3b1dd32c5e7
Maya Peneva <m.peneva@onebitsoftware.net>
2026-02-11 11:45:17 +0200

    Added a missed file to previous commit

--- Mayya-Peneva-employees.Client/Program.cs
+++ Mayya-Peneva-employees.Client/Program.cs
@@ -1,5 +1,16 @@
+using Mayya_Peneva_employees.Client.Core.Helpers.Converters;
+using Mayya_Peneva_employees.Client.Core.Helpers.Parsers;
+using Mayya_Peneva_employees.Client.Core.Services;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services.AddBlazorBootstrap();
+
+builder.Services.AddScoped<IDateParser, DateParser>();
+builder.Services.AddScoped<IEmployeeConverter, EmployeeConverter>();
+builder.Services.AddScoped<IEmployeeService, EmployeeService>();
+
+builder.Services.AddLogging(logging => logging.SetMinimumLevel(LogLevel.Information));
 
 await builder.Build().RunAsync();
